860d91f704522de68871644d5432302b7705e32f
Simon Jeffery <simonjeffery83@gmail.com>
2016-07-18 05:04:33 +1000

    events added

--- test.py
+++ test.py
@@ -4,9 +4,11 @@ import securities_master.retrieve_data as rd
 #from cointegration.cointegration import plot_scatter_series
 from cointegration.cointegration import cointegrate
 import forecast.forecast as f
-import performance_management from 
+import performance_management.performance_management as pm 
+import risk_management.risk_management as rm
 data1 = rd.get_data_from_ticker('CBA')
-#data2= rd.get_data_from_ticker('CBA')
+data2= rd.get_data_from_ticker('WBC')
+
 #test = Test_Stationarity(data,'close_price')
 #print(str(test.series))
 #test.dickey_fuller_test()
@@ -15,10 +17,13 @@ data1 = rd.get_data_from_ticker('CBA')
 #plot_price_series(data1,'close_price',data2,'close_price')
 #plot_scatter_series(data1,'close_price',data2,'close_price')
 #cointegrate('1PG',data1,'close_price','CBA',data2,'close_price')
-lagged_time_series = f.create_lagged_series(data1,'close_price',lags=2)
+#lagged_time_series = f.create_lagged_series(data1,'close_price',lags=2)
 
+#print(pm.annualised_sharpe(data1,'close_price'))
+#print(pm.equity_sharpe(data1,'close_price'))
+#print(pm.market_neutral_sharpe(data1,'close_price',data2,'close_price'))
 
-
+print(rm.var_cov_var(70000,data1,'close_price'))
 #print(lagged_time_series)
 
 #f.forecast(lagged_time_series,['Today','Lag_1%','Lag_2%'],'Direction',0.7,0.3)
